cb68516e071f7f40c049cae169eb7e3d5b6943b1
ileri2020 <adepojuololade2020@gmail.com>
2025-10-29 21:57:13 +0100

    links to pages

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,12 +6,18 @@ export default function Hero() {
   return (
     <section className="text-center py-20 px-6 md:px-12">
       <h1 className="text-4xl md:text-5xl font-bold mb-4">
-        Hospital Management System
+        UITH Hospital Management System
       </h1>
       <p className="text-muted-foreground max-w-2xl mx-auto text-lg mb-8">
-        Efficiently manage patients, appointments, and staff using our secure,
-        user-friendly platform built with Next.js, Tailwind CSS, and Supabase.
+        Efficiently manage patients, appointments, and staff.
       </p>
+      <div className='flex flex-col md:flex-row gap-2 mx-auto max-w-lg my-3 justify-center items-center'>
+        <Link href={'/patient'}><Button>Patient</Button></Link>
+        <Link href={'/doctor'}><Button>Doctor</Button></Link>
+        <Link href={'/nurse'}><Button>Nurse</Button></Link>
+        <Link href={'/pharmacy'}><Button>Phamacy</Button></Link>
+        {/* <Link href={'/admin'}><Button>Admin</Button></Link> */}
+      </div>
       <div className="flex justify-center gap-2">
         <Button asChild variant="secondary" size="lg">
           <Link href="/sign-in">Sign in</Link>
