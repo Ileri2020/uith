d778e3a06710ab1362db7c1b76b76fb2947b8645
Adepoju Ololade Joshua Ilerioluwa <adepojuololade2020@gmail.com>
2025-11-06 22:41:00 +0100

    annoying

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// @ts-nocheck
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   webpack: (config : any) => {
     config.module.rules.push({
       test: /\.wasm$/,
@@ -7,9 +9,28 @@ const nextConfig = {
     });
     return config;
   },
+  /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // webpack5: false,
+
+  // serverExternalPackages: ["mongoose"],
+
+  // experimental: {
+  //   // appDir: true,
+  //   esmExternals: "loose", // <-- add this
+  //   serverComponentsExternalPackages: ["mongoose"] // <-- and this
+  // },
+
+  // and the following to enable top-level await support for Webpack
+  // webpack: (config) => {
+  //   config.experiments = {
+  //     ...config.experiments,
+  //     topLevelAwait: true
+  //   };
+  //   return config;
+  // },
 };
 
 export default nextConfig;
