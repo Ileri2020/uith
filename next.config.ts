970a5b119f949a2303106cd549adba5ffac08cc8
Adepoju Ololade Joshua Ilerioluwa <adepojuololade2020@gmail.com>
2025-11-06 22:46:14 +0100

    turbopack replaces webpack

--- next.config.ts
+++ next.config.ts
@@ -2,35 +2,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config : any) => {
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: 'asset/resource',
-    });
-    return config;
-  },
-  /* config options here */
+  // Tell Next.js you’re using Turbopack (empty object silences the warning)
+  turbopack: {},
+
+  // ESLint settings
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // webpack5: false,
 
-  // serverExternalPackages: ["mongoose"],
-
-  // experimental: {
-  //   // appDir: true,
-  //   esmExternals: "loose", // <-- add this
-  //   serverComponentsExternalPackages: ["mongoose"] // <-- and this
-  // },
-
-  // and the following to enable top-level await support for Webpack
-  // webpack: (config) => {
-  //   config.experiments = {
-  //     ...config.experiments,
-  //     topLevelAwait: true
-  //   };
-  //   return config;
-  // },
+  // Any other config you still need…
+  // experimental: { … }
 };
 
 export default nextConfig;
