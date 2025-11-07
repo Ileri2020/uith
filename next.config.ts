c19ed151c61d3a24f816b620090a69ffa3bcd69e
Adepoju Ololade Joshua Ilerioluwa <adepojuololade2020@gmail.com>
2025-11-07 12:44:17 +0100

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,42 @@
-// @ts-nocheck
+// // @ts-nocheck
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   webpack: (config : any) => {
+//     config.module.rules.push({
+//       test: /\.wasm$/,
+//       type: 'asset/resource',
+//     });
+//     return config;
+//   },
+//   /* config options here */
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   // webpack5: false,
+
+//   // serverExternalPackages: ["mongoose"],
+
+//   // experimental: {
+//   //   // appDir: true,
+//   //   esmExternals: "loose", // <-- add this
+//   //   serverComponentsExternalPackages: ["mongoose"] // <-- and this
+//   // },
+
+//   // and the following to enable top-level await support for Webpack
+//   // webpack: (config) => {
+//   //   config.experiments = {
+//   //     ...config.experiments,
+//   //     topLevelAwait: true
+//   //   };
+//   //   return config;
+//   // },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config : any) => {
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: 'asset/resource',
-    });
-    return config;
-  },
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
